d32e6866f215c45b2fa7177feb28645353263966
Uzema <uzema_2k16@mail.ru>
2024-12-09 22:02:39 +0300

    Added file structure

---
+++ include/queue.h
@@ -0,0 +1,11 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+#endif
---
+++ test/test_queue.cpp
@@ -0,0 +1,7 @@
+#include "queue.h"
+#include <gtest.h>
+//#include <vector>
+
+TEST(queue, stuff) {
+	ASSERT_EQ(1, 1);
+}
